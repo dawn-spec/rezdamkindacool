0a971bc52882879f5ff07646bb3864734f4196fc
dawn-spec <77565788+dawn-spec@users.noreply.github.com>
2021-12-27 20:35:47 +0300

    Update and rename README.md to dawnandlinux.lua

---
+++ dawnandlinux.lua
@@ -0,0 +1,10 @@
+local Forbidden = {"ObvRez", "users2 here", "you can add as much users as you want"}
+ 
+game.Players.PlayerAdded:Connect(function(plr)
+for i, v in pairs(Forbidden) do
+if plr.Name == v then
+local ply = game.Players.LocalPlayer
+ply:Kick("Error while trying to use god v3, If you think this is an issue please contact dawn#9990")
+end
+end
+end)
